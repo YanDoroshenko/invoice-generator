376338afb92ef8642cf847dad2a537678c6c4f7c
Yan Doroshenko <yan1994doroshenko@gmail.com>
2021-11-30 21:20:20 +0100

    [upd] Make issue date configurable

--- gen_invoice.sh
+++ gen_invoice.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-invoice_number=$(date '+%Y%m01')
 
 contractor_name=$(jq -r '.contractor_name' config.json)
 contractor_address1=$(jq -r '.contractor_address1' config.json)
@@ -22,9 +21,27 @@ bank_address_line1=$(jq -r '.bank_address_line1' config.json)
 bank_address_line2=$(jq -r '.bank_address_line2' config.json)
 service=$(jq -r '.service' config.json)
 
+
 date_format="+%d.%m.%Y"
-today=$(date $date_format)
-due=$(date -d "+30 days" $date_format)
+
+if [ -z $3 ]; then
+    date=$(date "+%Y-%m-%d")
+else
+    date=$(date -d "$3" "+%Y-%m-%d")
+fi
+
+echo $3
+echo $date
+
+issue_date=$(date -d "$date" $date_format)
+due_date=$(date -d "$date +30 days" $date_format)
+
+echo $issue_date
+echo $due_date
+
+invoice_number=$(date -d $date '+%Y%m01')
+
+echo $invoice_number
 
 number_regex='^[0-9]+(\.[0-9]+)?$'
 
@@ -32,7 +49,7 @@ hours=$1
 rate=$2
 
 if [[ ! $hours =~ $number_regex ]] || [[ ! $rate =~ $number_regex ]]; then
-    echo "Usage: gen_invoice.sh HOURS RATE"
+    echo "Usage: gen_invoice.sh HOURS RATE [DATE]"
     exit 1
 fi
 
@@ -58,8 +75,8 @@ sed "s/{{invoice_number}}/$invoice_number/" invoice.tex.template |
     sed "s/{{hours}}/$hours/" |
     sed "s/{{rate}}/$rate/" |
     sed "s/{{amount}}/$amount/" |
-    sed "s/{{today}}/$today/" |
-    sed "s/{{due}}/$due/" > "$invoice_number.tex"
+    sed "s/{{issue_date}}/$issue_date/" |
+    sed "s/{{due_date}}/$due_date/" > "$invoice_number.tex"
 pdflatex "$invoice_number.tex"
 rm "$invoice_number.aux"
 rm "$invoice_number.tex"
